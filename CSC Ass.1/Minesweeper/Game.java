32a2541123f4fcff65a40ec93446cb5cca543bf6
JoshbutMagma <HunteJo@whs.school.nz>
2024-06-26 11:07:39 +1200

    Seventeenth Upload
    minor changes, such as fixing a bug where ending the game on a bomb counts as a win, and adding an easy way to change the board size and how many bombs there are

--- CSC Ass.1/Minesweeper/Game.java
+++ CSC Ass.1/Minesweeper/Game.java
@@ -1,30 +1,42 @@
 
 /**
  * Play a game of Minesweeper
+ * By default, it uses Google Minesweeper easy settings (10/8 board, 10 bombs), however it's easily changeable in the code by replacing the values for "bombs", "X", and "Y"
  *
  * Joshua Hunter
  * 06/05-
+ * 
+ * Suggestions:
+ * Try formatting the numbers on the board better (not starting at 0, using letters, etc)
  */
 import java.util.Scanner;
 import java.util.Random;
 public class Game
 {
-    static void print(String[][] board)
+    static void print(String[][] board, int X, int Y)
     {
         System.out.print("  ");
-        for (int i=0; i<10; i++){
+        for (int i=0; i<X; i++){
             System.out.print(i + " ");
         }
         System.out.println();
-        for (int i=1; i<9; i++){
-            for (int j=1; j<11; j++){
+        for (int i=1; i<(Y+1); i++){
+            for (int j=1; j<(X+1); j++){
                 if(j==1){
                     System.out.print((i-1) + " ");
                 }
                 System.out.print(board[j][i] + " ");
+                if(j==X){
+                    System.out.print(i-1);
+                }
             }
             System.out.println();
         }
+        System.out.print("  ");
+        for (int i=0; i<X; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
     static int input(int xTest)
     {
@@ -38,18 +50,15 @@ public class Game
     }
     public static void main(String[] args)
     {
-        /**
-         * Replace hard values with variables
-         * Try formatting the numbers on the board better (not starting at 0, using letters, etc)
-         * Fix bug where ending the game on a bomb counts as victory
-         */
         //innitializing variables, scanner, and board
         System.out.print('\u000c');
+        int bombs = 10;
+        int X = 10;
+        int Y = 8;
         Scanner keyboard = new Scanner(System.in);
         Random random = new Random();
-        String[][] board = new String[12][10];
-        String[][] underBoard = new String[12][10];
-        int bombs = 5;
+        String[][] board = new String[X+2][Y+2];
+        String[][] underBoard = new String[X+2][Y+2];
         int bombX;
         int bombY;
         int num = 0;
@@ -64,26 +73,26 @@ public class Game
         boolean gameRunning = true;
         
         //Creating the initial board
-        for (int i=1; i<9; i++){
-            for (int j=1; j<11; j++){
+        for (int i=1; i<(Y+1); i++){
+            for (int j=1; j<(X+1); j++){
                 board[j][i] = "■";
                 underBoard[j][i] = "·";
             }
         }
-        print(board);
+        print(board,X,Y);
         
         //Randomly planting bombs
         while (bombsPlaced<bombs){
-            bombX = random.nextInt(10)+1;
-            bombY = random.nextInt(8)+1;
+            bombX = random.nextInt(X)+1;
+            bombY = random.nextInt(Y)+1;
             if(underBoard[bombX][bombY]!="💣"){
                 underBoard[bombX][bombY] = "💣";
                 bombsPlaced++;
             }
         }
         //Fill the board with numbers. j and i allow the program to repeat on every tile. Then, it checks if the cell is a bomb, and adds +1 to each nearby square.
-        for (int i=1; i<9; i++){
-            for (int j=1; j<11; j++){
+        for (int i=1; i<(Y+1); i++){
+            for (int j=1; j<(X+1); j++){
                 if (underBoard[j][i]!="💣"){
                     for (int k=0; k<3; k++){
                         for (int l=0; l<3; l++){
@@ -98,8 +107,8 @@ public class Game
             }
         }
         //Replace every 0 with a "·"
-        for (int i=1; i<9; i++){
-            for (int j=1; j<11; j++){
+        for (int i=1; i<(Y+1); i++){
+            for (int j=1; j<(X+1); j++){
                 if (underBoard[j][i].equals("0")){
                     underBoard[j][i] = "·";
                 }
@@ -120,7 +129,7 @@ public class Game
             System.out.print('\u000c');
             
             //This "if" bracket around everything prevents invalid integers crashing the game
-            if(xTest<11&&xTest>-1&&yTest<9&&yTest>-1){
+            if(xTest<(X+1)&&xTest>-1&&yTest<(Y+1)&&yTest>-1){
                 //Reveal the corresponding square that was entered, and print the new board
                 if(method.equals("T")&&!(board[xTest][yTest].equals("⚑"))){
                     board[xTest][yTest] = underBoard[xTest][yTest];
@@ -136,8 +145,8 @@ public class Game
                 
                 //Clears tiles automatically. c repeats the program so that the board is fully able to clear from the bottom right corner to the top left if needed. j and i check every tile. The program then checks if a tile is blank, and reveals any tile in an area around the blank.
                 for(int c=0; c<15; c++){
-                    for (int i=1; i<9; i++){
-                        for (int j=1; j<11; j++){
+                    for (int i=1; i<(Y+1); i++){
+                        for (int j=1; j<(X+1); j++){
                             if (board[j][i]=="·"){
                                 for (int k=0; k<3; k++){
                                     for (int l=0; l<3; l++){
@@ -152,27 +161,27 @@ public class Game
                 }
                 
                 //Checks if any tile is still covered up, and stores that to determine if the game is won.
-                for(int i=1; i<9; i++){
-                    for(int j=1; j<11; j++){
-                        if(board[j][i]=="■"){
+                for(int i=1; i<(Y+1); i++){
+                    for(int j=1; j<(X+1); j++){
+                        if(board[j][i]=="■"||board[j][i]=="💣"){
                             tilesLeft++;
                         }
                     }
                 }
                 
                 if(!board[xTest][yTest].equals("💣")&&tilesLeft!=0){
-                    print(board);
+                    print(board,X,Y);
                     System.out.println("You've placed " + flagsPlaced + "/" + bombs + " flags.");
                 }
                 
                 //Was that turn Game Over?
                 if(board[xTest][yTest].equals("💣")){
-                    print(board);
+                    print(board,X,Y);
                     System.out.println("Game over");
                     gameRunning = false;
                 }
                 if(tilesLeft==0){
-                    print(board);
+                    print(board,X,Y);
                     System.out.println("You win! Congratulations! You won in " + turn + " turns.");
                     gameRunning = false;
                 }
@@ -180,7 +189,7 @@ public class Game
                 tilesLeft = 0;
             }else{
                 System.out.println("Bad input, try again");
-                print(board);
+                print(board,X,Y);
                 System.out.println("You've placed " + flagsPlaced + "/" + bombs + " flags.");
             }
         }

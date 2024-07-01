0f93d9cfdb6ea2fa30c95ea1307b408bad1467de
JoshbutMagma <HunteJo@whs.school.nz>
2024-07-01 15:14:32 +1200

    Eighteenth upload
    Added several notes

--- CSC Ass.1/Minesweeper/Game.java
+++ CSC Ass.1/Minesweeper/Game.java
@@ -4,7 +4,7 @@
  * By default, it uses Google Minesweeper easy settings (10/8 board, 10 bombs), however it's easily changeable in the code by replacing the values for "bombs", "X", and "Y"
  *
  * Joshua Hunter
- * 06/05-
+ * 06/05
  * 
  * Suggestions:
  * Try formatting the numbers on the board better (not starting at 0, using letters, etc)
@@ -13,8 +13,13 @@ import java.util.Scanner;
 import java.util.Random;
 public class Game
 {
+    /**
+     * This method goes allows the board to print by looking at every tile and printing what it is
+     * The method intakes the current board state as a 2D string array, as well as the values of how large the board is as integers
+     */
     static void print(String[][] board, int X, int Y)
     {
+        //This loop, and the similar one below, prints numbers at the top of the screen for reference when inputting a tile
         System.out.print("  ");
         for (int i=0; i<X; i++){
             System.out.print(i + " ");
@@ -22,6 +27,7 @@ public class Game
         System.out.println();
         for (int i=1; i<(Y+1); i++){
             for (int j=1; j<(X+1); j++){
+                //This loop, and the similar one below, prints the numbers on the side for reference when inputting a tile
                 if(j==1){
                     System.out.print((i-1) + " ");
                 }
@@ -38,16 +44,24 @@ public class Game
         }
         System.out.println();
     }
+    /**
+     * This method determines whether or not an input is an integer, or if it isn't and needs to be re-inputted
+     * The method intakes an integer to use for testing
+     * The method returns the valid integer
+     */
     static int input(int xTest)
     {
         Scanner Keyboard = new Scanner(System.in);
         while(!Keyboard.hasNextInt()){
-            System.out.println("Input error");
+            System.out.println("Input error, please try again");
             Keyboard.nextLine();
         }
         int num = Keyboard.nextInt();
         return num;
     }
+    /**
+     * This is where the game is made and played
+     */
     public static void main(String[] args)
     {
         //innitializing variables, scanner, and board
@@ -72,7 +86,7 @@ public class Game
         String method;
         boolean gameRunning = true;
         
-        //Creating the initial board
+        //Creating the initial board. The board is what is played on and the underBoard contains the bombs and numbers
         for (int i=1; i<(Y+1); i++){
             for (int j=1; j<(X+1); j++){
                 board[j][i] = "■";
@@ -85,15 +99,16 @@ public class Game
         while (bombsPlaced<bombs){
             bombX = random.nextInt(X)+1;
             bombY = random.nextInt(Y)+1;
-            if(underBoard[bombX][bombY]!="💣"){
+            if(underBoard[bombX][bombY]!="💣"){//This checks for if a bomb is already in a tile, so that we do have exactly X bombs instead of doubling up
                 underBoard[bombX][bombY] = "💣";
                 bombsPlaced++;
             }
         }
-        //Fill the board with numbers. j and i allow the program to repeat on every tile. Then, it checks if the cell is a bomb, and adds +1 to each nearby square.
+        //Fill the board with numbers. j and i allow the program to repeat on every tile
         for (int i=1; i<(Y+1); i++){
             for (int j=1; j<(X+1); j++){
                 if (underBoard[j][i]!="💣"){
+                    //Then, it checks each nearby cell for if it's a bob, and adds +1 to itself
                     for (int k=0; k<3; k++){
                         for (int l=0; l<3; l++){
                             if (underBoard[(j+k-1)][(i+l-1)]=="💣"){
@@ -128,8 +143,8 @@ public class Game
             yTest = input(num)+1;
             System.out.print('\u000c');
             
-            //This "if" bracket around everything prevents invalid integers crashing the game
-            if(xTest<(X+1)&&xTest>-1&&yTest<(Y+1)&&yTest>-1){
+            //This if bracket around everything prevents invalid integers crashing the game
+            if(method.equals("T")||method.equals("F")&&xTest<(X+1)&&xTest>-1&&yTest<(Y+1)&&yTest>-1){
                 //Reveal the corresponding square that was entered, and print the new board
                 if(method.equals("T")&&!(board[xTest][yTest].equals("⚑"))){
                     board[xTest][yTest] = underBoard[xTest][yTest];
@@ -139,14 +154,13 @@ public class Game
                 }else if(method.equals("F")&&board[xTest][yTest].equals("⚑")){
                     board[xTest][yTest] = "■";
                     flagsPlaced--;
-                }else{
-                    System.out.println("Bad input, try again");
                 }
                 
-                //Clears tiles automatically. c repeats the program so that the board is fully able to clear from the bottom right corner to the top left if needed. j and i check every tile. The program then checks if a tile is blank, and reveals any tile in an area around the blank.
+                //Clears tiles automatically. c repeats the program so that the board is fully able to clear from the bottom right corner to the top left if needed. j and i check every tile
                 for(int c=0; c<15; c++){
                     for (int i=1; i<(Y+1); i++){
                         for (int j=1; j<(X+1); j++){
+                            //The program then checks if a tile is blank, and reveals any tile in an area around the blank.
                             if (board[j][i]=="·"){
                                 for (int k=0; k<3; k++){
                                     for (int l=0; l<3; l++){
@@ -160,7 +174,7 @@ public class Game
                     }
                 }
                 
-                //Checks if any tile is still covered up, and stores that to determine if the game is won.
+                //Checks if any tile is still covered up, and stores that to determine if the game is won
                 for(int i=1; i<(Y+1); i++){
                     for(int j=1; j<(X+1); j++){
                         if(board[j][i]=="■"||board[j][i]=="💣"){
@@ -169,12 +183,13 @@ public class Game
                     }
                 }
                 
+                //Prints the board and flag count if the game is still running
                 if(!board[xTest][yTest].equals("💣")&&tilesLeft!=0){
                     print(board,X,Y);
                     System.out.println("You've placed " + flagsPlaced + "/" + bombs + " flags.");
                 }
                 
-                //Was that turn Game Over?
+                //Was that turn Game Over? This checks for and outputs if a win or loss happens
                 if(board[xTest][yTest].equals("💣")){
                     print(board,X,Y);
                     System.out.println("Game over");
@@ -188,6 +203,7 @@ public class Game
                 turn++;
                 tilesLeft = 0;
             }else{
+                //If the input doesn't work, 
                 System.out.println("Bad input, try again");
                 print(board,X,Y);
                 System.out.println("You've placed " + flagsPlaced + "/" + bombs + " flags.");
